ca3ce088964cc3c29978b3a2fd60b1a37026fcf7
nitishshar <nitishshar@gmail.com>
2021-03-11 02:38:50 +0530

    Initial Commit

---
+++ B2CGraphClient/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace B2CGraphShell
+{
+    public class User
+    {
+        public string Id { get; set; }
+        public string DisplayName { get; set; }
+        public List<Role> Roles { get; set; } = new List<Role>();
+        public string EmailId { get; set; }
+    }
+
+    public class Role
+    {
+        public string RoleId { get; set; }
+        public string RoleName { get; set; }
+        public string RoleDescription { get; set; }
+    }
+}
